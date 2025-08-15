aaaea34a5eb7e97c271779a31bca635344bd2b8c
ryanMicrosoftTest <admin@mngenvmcap372892.onmicrosoft.com>
2025-08-15 15:47:25 +0000

    Committing 1 item from workspace a8cbda3d-903e-4154-97d9-9a91c95abb42

--- fabric_items/create_metadata_tables.Notebook/notebook-content.py
+++ fabric_items/create_metadata_tables.Notebook/notebook-content.py
@@ -41,13 +41,15 @@ from pyspark.sql.types import StructType, StructField, StringType
 
 
 shortcut_table_csv_data = """
-healthdata,Files,AdlsGen2,1f126619-984e-4ab2-b5fa-42d5680242f7,https://healthsaadlsrheus.dfs.core.windows.net,/healthdata,Folder
-dicom,Files,AdlsGen2,1f126619-984e-4ab2-b5fa-42d5680242f7,https://healthsaadlsrheus.dfs.core.windows.net,/dicom,Folder
+healthdata,a8cbda3d-903e-4154-97d9-9a91c95abb42,4bd2cd19-0177-4fa9-9ab7-a67a69233f72,Files,AdlsGen2,1f126619-984e-4ab2-b5fa-42d5680242f7,https://healthsaadlsrheus.dfs.core.windows.net,/healthdata,Folder
+dicom,a8cbda3d-903e-4154-97d9-9a91c95abb42,4bd2cd19-0177-4fa9-9ab7-a67a69233f72,Files,AdlsGen2,1f126619-984e-4ab2-b5fa-42d5680242f7,https://healthsaadlsrheus.dfs.core.windows.net,/dicom,Folder
 """
 
 
 shortcut_table_schema = StructType([
     StructField("name", StringType(), True),
+    StructField("workspace_id", StringType(), True),
+    StructField("lakehouse_id", StringType(), True),
     StructField("shortcut_location", StringType(), True),
     StructField("target_type", StringType(), True),
     StructField("target_connection_id", StringType(), True),
@@ -71,8 +73,6 @@ data_rows = [row.split(",") for row in shortcut_table_csv_data.strip().split("\n
 # CELL ********************
 
 shortcut_df = spark.createDataFrame(data_rows, schema=shortcut_table_schema)
-
-
 
 # METADATA ********************
 
